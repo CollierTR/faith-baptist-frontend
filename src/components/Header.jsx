1991894fb84e86e98b56c97de82241e103e87008
Strykerboy <lukertrammel@gmail.com>
2025-11-15 12:50:04 -0600

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
+import { faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function Header() {
   return (
-    <div>
-      <p>Header</p>
-      <p>Header</p>
+    <div class="p-3 flex justify-between">
+      <div>
+        <h1 class="text-2xl font-bold">FAITH BAPTIST CHURCH</h1>
+      </div>
+      <div class="text-xl">
+        <a href="/index" class="p-3 hover:text-secondary-dark">HOME</a>
+        <a href="/about" class="p-3 hover:text-secondary-dark">ABOUT</a>
+        <a href="/sermons" class="p-3 hover:text-secondary-dark">SERMONS</a>
+        <FontAwesomeIcon icon={faHamburger} />
+      </div>
     </div>
   );
 }
