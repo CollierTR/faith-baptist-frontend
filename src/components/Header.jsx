f3203ad57dde3ca6e33f383a99fa88d85e8426af
Strykerboy <lukertrammel@gmail.com>
2025-11-15 14:36:12 -0600

    add drop down menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,30 @@
-import { faHamburger } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const [navOpen, toggleNav] = useState(false)
   return (
-    <div class="p-3 flex justify-between">
+    <div className="p-3 flex justify-between relative">
       <div>
-        <h1 class="text-2xl font-bold">FAITH BAPTIST CHURCH</h1>
+        <h1 className="text-2xl font-bold"><a href="/">FAITH BAPTIST CHURCH</a></h1>
       </div>
-      <div class="text-xl">
-        <a href="/index" class="p-3 hover:text-secondary-dark">HOME</a>
-        <a href="/about" class="p-3 hover:text-secondary-dark">ABOUT</a>
-        <a href="/sermons" class="p-3 hover:text-secondary-dark">SERMONS</a>
-        <FontAwesomeIcon icon={faHamburger} />
+      <div className="text-xl">
+        <a href="/" className="p-3 hover:text-secondary-dark">HOME</a>
+        <a href="/about" className="p-3 hover:text-secondary-dark">ABOUT</a>
+        <a href="/sermons" className="pl-3 pr-6 hover:text-secondary-dark">SERMONS</a>
+        <FontAwesomeIcon onClick={() => {toggleNav(true)}} icon={faBars} />
       </div>
+      { 
+        navOpen &&
+        <div className="bg-primary text-primary-dark absolute flex flex-col gap-5 top-0 right-0 ">
+          <FontAwesomeIcon onClick={() => {toggleNav(false)}} icon={faX} />
+          <a href="">BIBLICAL COUNSELING</a>
+          <a href="">RESOURCES</a>
+          <a href="/contact">CONTACT</a>
+        </div>
+      }
     </div>
   );
 }
