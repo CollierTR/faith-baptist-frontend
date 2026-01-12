ff80b04d3c19084ef41ecec45a95349c234f31e9
CollierTR <tristancollier777@gmail.com>
2026-01-11 22:10:48 -0600

    update the sermons cards

--- src/components/sermons/LoadMoreSermons.jsx
+++ src/components/sermons/LoadMoreSermons.jsx
@@ -18,16 +18,21 @@ export default function LoadMoreSermons() {
   }, [page]);
 
   return (
-    <div>
-      <div className={"flex flex-col gap-4"}>
+    <div className={"flex flex-col place-items-center gap-20 mx-auto"}>
+      <div className={"grid grid-cols-3 gap-4"}>
         {sermons.map(sermon => (
-          <div key={sermon.id} className={'bg-amber-800'}>
-            <a href={`/sermons/${sermon.slug}/`} dangerouslySetInnerHTML={{ __html: sermon.title.rendered }}></a>
-            <p>{sermon.date}</p>
+          <div key={sermon.id} className={'bg-white dark:bg-gray-800 shadow-md rounded-lg mb-4 flex flex-col justify-between leading-normal'}>
+            <img src="/imgs/bible.png" alt="" className={"overflow-hidden"}/>
+            <div className="p-2">
+              <div className="mb-2">
+                <a href={`/sermons/${sermon.slug}/`} className="text-gray-900 dark:text-white font-bold text-xl mb-2 hover:text-amber-700" dangerouslySetInnerHTML={{ __html: sermon.title.rendered }}></a>
+              </div>
+              <p className="text-gray-600 dark:text-gray-400 text-base">{new Date(sermon.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
+            </div>
           </div>
         ))}
       </div>
-      <button onClick={() => setPage(page + 1)} disabled={loading} className={'bg-amber-400'}>
+      <button onClick={() => setPage(page + 1)} disabled={loading} className={'mt-4 px-6 py-3 bg-amber-600 hover:bg-amber-700 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out disabled:opacity-50'}>
         {loading ? 'Loading...' : 'Load More'}
       </button>
     </div>
