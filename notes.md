54aa7555cd3fb40033dd689fd87f4af23d0a7e8b
CollierTR <tristancollier777@gmail.com>
2026-01-13 21:51:43 -0600

    update sermon page

---
+++ notes.md
@@ -0,0 +1,2 @@
+API Endpoint for sermon Categories
+"https://media.faithbaptistkirksville.org/wp-json/wp/v2/categories?parent=3&per_page=100"
--- src/components/sermons/LoadMoreSermons.jsx
+++ src/components/sermons/LoadMoreSermons.jsx
@@ -18,19 +18,28 @@ export default function LoadMoreSermons() {
   }, [page]);
 
   return (
-    <div className={"flex flex-col place-items-center gap-20 mx-auto"}>
-      <div className={"grid grid-cols-3 gap-4"}>
+    <div className={"flex flex-col place-items-center gap-20"}>
+      <div className={"flex flex-col gap-8 w-full"}>
+
         {sermons.map(sermon => (
-          <div key={sermon.id} className={'bg-white dark:bg-gray-800 shadow-md rounded-lg mb-4 flex flex-col justify-between leading-normal'}>
-            <img src="/imgs/bible.png" alt="" className={"overflow-hidden"}/>
+          <div key={sermon.id} className={'w-full bg-white rounded-md mb-4 flex flex-col justify-between leading-normal shadow-lg drop-shodow-lg'}>
+            <div className="flex">
+              <a href={`/sermons/${sermon.slug}/`} className="text-black font-bold text-xl mb-2 hover:text-amber-700" dangerouslySetInnerHTML={{ __html: sermon.title.rendered }}></a>
+            </div>
+
+            {/* 
             <div className="p-2">
               <div className="mb-2">
-                <a href={`/sermons/${sermon.slug}/`} className="text-gray-900 dark:text-white font-bold text-xl mb-2 hover:text-amber-700" dangerouslySetInnerHTML={{ __html: sermon.title.rendered }}></a>
+                <a href={`/sermons/${sermon.slug}/`} className="text-black font-bold text-xl mb-2 hover:text-amber-700" dangerouslySetInnerHTML={{ __html: sermon.title.rendered }}></a>
               </div>
               <p className="text-gray-600 dark:text-gray-400 text-base">{new Date(sermon.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
             </div>
+            */}
+
           </div>
         ))}
+
+
       </div>
       <button onClick={() => setPage(page + 1)} disabled={loading} className={'mt-4 px-6 py-3 bg-amber-600 hover:bg-amber-700 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out disabled:opacity-50'}>
         {loading ? 'Loading...' : 'Load More'}
