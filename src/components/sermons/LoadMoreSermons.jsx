1a2dd7c8bb414e5ec5db03fd19caa641bd7fd672
Tristan Collier <80648892+CollierTR@users.noreply.github.com>
2026-08-15 19:48:07 -0700

    fix: show proper speaker on sermon cards (#6)
    * feat (content): add events page
    
    * feat: finish event page
    
    * fix: show proper speaker on sermon cards
    
    * fix: fix merge conflicts

--- src/components/sermons/LoadMoreSermons.jsx
+++ src/components/sermons/LoadMoreSermons.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
-import { format, parseISO } from 'date-fns';
-import speakers from "../../data/speakers.js"
+import { useState, useEffect } from "react";
+import { format, parseISO } from "date-fns";
+import { authors } from "../../data/authors.js";
 
-export default function LoadMoreSermons({mode, value}) {
-  console.log(mode, value)
+export default function LoadMoreSermons({ mode, value }) {
+  console.log(mode, value);
   const [sermons, setSermons] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
@@ -12,7 +12,7 @@ export default function LoadMoreSermons({mode, value}) {
   let filter;
   switch (mode) {
     case "speaker":
-      filter = `&user=${value}`;
+      filter = "&categories=3";
       break;
     case "series":
       filter = `&categories=${value}`;
@@ -24,48 +24,86 @@ export default function LoadMoreSermons({mode, value}) {
       filter = "&categories=3";
   }
 
-
   const fetchSermons = async () => {
     setLoading(true);
-    const res = await fetch(`https://media.faithbaptistkirksville.org/wp-json/wp/v2/media?per_page=50&page=${page}&orderby=date&order=desc${filter}`);
+    const res = await fetch(
+      `https://media.faithbaptistkirksville.org/wp-json/wp/v2/media?per_page=50&page=${page}&orderby=date&order=desc${filter}`,
+    );
     const newSermons = await res.json();
-    setSermons(prevSermons => [...prevSermons, ...newSermons]);
-    console.log(newSermons)
+    const filteredSermons =
+      mode === "speaker"
+        ? newSermons.filter(
+            (sermon) => String(sermon.author) === String(value),
+          )
+        : newSermons;
+    setSermons((prevSermons) => [...prevSermons, ...filteredSermons]);
+    console.log(newSermons);
     setLoading(false);
   };
+
+  function listAuthor(author) {
+    if (author === 2) {
+      return "Brandon Rhea";
+    } else if (author === 3) {
+      return "Jon";
+    }
+  }
 
   useEffect(() => {
     fetchSermons();
   }, [page]);
 
   return (
-    <div className={"flex flex-col place-items-center gap-10 text-primary-dark"}>
+    <div
+      className={"flex flex-col place-items-center gap-10 text-primary-dark"}
+    >
       <div className={"flex flex-col gap-10 w-full"}>
+        {sermons.map((sermon) => {
+          return (
+            <div
+              key={sermon.id}
+              className={
+                "w-full bg-white rounded-md p-10 flex flex-col gap-10 justify-between leading-normal shadow-lg drop-shodow-lg"
+              }
+            >
+              <div className="flex flex-col gap-1">
+                <p className=" font-extralight">
+                  {format(parseISO(sermon.date), "MMM d, yyyy")}
+                </p>
+                <p
+                  className="text-3xl font-semibold"
+                  dangerouslySetInnerHTML={{ __html: sermon.title.rendered }}
+                ></p>
+              </div>
 
-        {sermons.map(sermon => {
-          const speaker = speakers.find(s => s.id === sermon.author);
-        return(
-          <div key={sermon.id} className={'w-full bg-white rounded-md p-10 flex flex-col gap-10 justify-between leading-normal shadow-lg drop-shodow-lg'}>
-            <div className="flex flex-col gap-1">
-              <p className=" font-extralight">{format(parseISO(sermon.date), "MMM d, yyyy")}</p>
-              <p className="text-3xl font-semibold" dangerouslySetInnerHTML={{ __html: sermon.title.rendered }}></p>
+              <p>By: {authors[sermon.author]}</p>
+
+              <div className="flex gap-0.5 text-white">
+                <a href={sermon.source_url} target="_blank">
+                  <div className="bg-primary px-4 py-2 rounded-l-md">
+                    Listen
+                  </div>
+                </a>
+                <a
+                  href={`/api/download?src=${encodeURIComponent(sermon.source_url)}`}
+                >
+                  <div className="bg-primary px-4 py-2 rounded-r-md">
+                    Download
+                  </div>
+                </a>
+              </div>
             </div>
-
-            <p>By: Brandon Rhea{/*speaker ? speaker.name : "Unknown"*/}
-              </p>
-
-            <div className="flex gap-0.5 text-white"> 
-              <a href={sermon.source_url} target="_blank"><div className="bg-primary px-4 py-2 rounded-l-md">Listen</div></a>
-              <a href={`/api/download?src=${encodeURIComponent(sermon.source_url)}`}><div className="bg-primary px-4 py-2 rounded-r-md">Download</div></a>
-            </div>
-
-          </div>
-        )})}
-
-
+          );
+        })}
       </div>
-      <button onClick={() => setPage(page + 1)} disabled={loading} className={'my-10 px-6 py-3 bg-primary hover:bg-amber-700 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out disabled:opacity-50'}>
-        {loading ? 'Loading...' : 'Load More'}
+      <button
+        onClick={() => setPage(page + 1)}
+        disabled={loading}
+        className={
+          "my-10 px-6 py-3 bg-primary hover:bg-amber-700 text-white font-bold rounded-lg shadow-md transition duration-300 ease-in-out disabled:opacity-50"
+        }
+      >
+        {loading ? "Loading..." : "Load More"}
       </button>
     </div>
   );
